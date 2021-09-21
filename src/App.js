af12fca6d6b396dc426cba3a424f1d5183ba4c8f
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-09-21 22:37:35 +0300

    create and style Jumbotron component

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import './App.css';
-import Jumbotron from './components/Jumbotron';
-
-export default function App() {
-  return <Jumbotron />;
-}
--- src/components/Jumbotron.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Jumbotron({ direction = 'flex-row' }) {
-  return (
-    <div className={`flex ${direction} items-center justify-between w-full max-w-screen-lg md:flex-column`}>
-      <p>I am Jumbo</p>
-    </div>
-  );
-}
