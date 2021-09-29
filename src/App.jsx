8b25072a1f8b4d2cbe02c5ebf7c70be55df20555
Eimantas Andrejevas <eimutis.a@gmail.com>
2021-09-29 23:18:53 +0300

    add all routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,15 @@ export default function App() {
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <SignIn />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <SignUp />
+      </Route>
     </Router>
   );
 }
